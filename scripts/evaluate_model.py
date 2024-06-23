429be89d4e9aef6a4595bbbb370c872339951b4e
cormsemanor <vladimirchik19907@mail.ru>
2024-06-23 04:27:59 +0300

    added files

--- scripts/evaluate_model.py
+++ scripts/evaluate_model.py
@@ -5,7 +5,6 @@ from src.evaluate import evaluate_model
 if __name__ == "__main__":
     data_csv = "../data/CXR8/BBox_List_2017.csv"
     img_dir = "../data/CXR8/images/images"
-    num_classes = 8  # Update based on your dataset
+    num_classes = 8
 
     predictions = evaluate_model(data_csv, img_dir, num_classes)
-    # Save predictions if needed
